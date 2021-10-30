dba9431149a86f9a90b84f94d98656a068633cd0
Interios <Gobiacc@gmail.com>
2021-10-31 01:55:43 +0300

    create folder for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TerminalComponent } from './terminal/terminal.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
+import { TerminalItemComponent } from './components/terminal-item/terminal-item.component';
 import { ReversePipe } from './pipes/reverse/reverse.pipe';
 import { PositiveDirective } from './directives/positive/positive.directive';
-import { TerminalItemComponent } from './terminal-item/terminal-item.component';
 
 @NgModule({
   declarations: [
