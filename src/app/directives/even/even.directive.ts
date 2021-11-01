85c32bebcb3a9b024a2ddb93b56e649f08a77b1b
Interios <Gobiacc@gmail.com>
2021-11-01 16:41:18 +0200

    create directive for positive number

--- src/app/directives/even/even.directive.ts
+++ src/app/directives/even/even.directive.ts
@@ -8,7 +8,6 @@ export class EvenDirective {
 
   @HostListener('input', ['$event'])
   onKeyDown(event: KeyboardEvent) {
-    //this.el.nativeElement.value = [...this.el.nativeElement.value].filter((el: number) => el % 2 == 0).join("");
-    this.el.nativeElement.value = this.el.nativeElement.value.match(/\d*[02468]/g);
+    this.el.nativeElement.value = [...this.el.nativeElement.value].filter((el: number) => el % 2 == 0).join("");
   }
 }
